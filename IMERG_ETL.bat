750620564d00d233b804b5826f193656146be499
Githika Tondapu <geethu.githika@gmail.com>
2018-04-02 14:18:42 -0500

    Update IMERG_ETL.bat

--- IMERG_ETL.bat
+++ IMERG_ETL.bat
@@ -1,2 +1,2 @@
 C:\Python27\ArcGISx6410.4\python.exe D:\SERVIR\Scripts\IMERG\IMERG_ETL.py
-C:\Python27\ArcGISx6410.4\python.exe D:\SERVIR\Scripts\IMERG_Acc\IMERG_Acc_ETL.py
+C:\Python27\ArcGISx6410.4\python.exe D:\SERVIR\Scripts\IMERG\IMERG_Acc_ETL.py
