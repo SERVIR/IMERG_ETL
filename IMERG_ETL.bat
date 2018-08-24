9d4db1f38fb56e4a8e09243059378efea3cf7546
NDC\gtondapu <githika.tondapu@nasa.gov>
2018-08-24 09:45:26 -0500

    added ftp code

--- IMERG_ETL.bat
+++ IMERG_ETL.bat
@@ -1,2 +1,2 @@
+C:\Python27\ArcGISx6410.4\python.exe D:\SERVIR\Scripts\IMERG\IMERG_ETL.py
 C:\Python27\ArcGISx6410.4\python.exe D:\SERVIR\Scripts\IMERG\IMERG_Acc_ETL.py
-set /p finishProcessing=Are you done with this window [y/n]?: 
